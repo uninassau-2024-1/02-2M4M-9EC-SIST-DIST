9226576b01ff4150914668151a8faef2c7d5ff75
nathanhs002@gmail.com <nathanhs002@gmail.com>
2024-04-02 12:03:08 -0300

    create : sidi-grupo3 completo

---
+++ sidi-grupo3/frontend/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ViacepComponent } from './components/viacep/viacep.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: ViacepComponent,
+  },
+];
